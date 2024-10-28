66edd6b1cfc5d4071195ac5052b7ee6f874eb999
light <65517797+liteless@users.noreply.github.com>
2024-10-28 16:11:34 -0400

    add hashing + salting functionality (#36)

--- src/app/register/actions.tsx
+++ src/app/register/actions.tsx
@@ -1,21 +1,29 @@
 "use server";
 
 import { db } from "@/db/index";
-import { teams, interactionModeEnum } from "@/db/schema";
+import { teams, type interactionModeEnum } from "@/db/schema";
+import { hash } from "bcrypt";
 
-export async function insertTeam(username: string, displayName: string, password: string, interactionMode: (typeof interactionModeEnum.enumValues)[number]) {
+export async function insertTeam(
+  username: string,
+  displayName: string,
+  password: string,
+  interactionMode: (typeof interactionModeEnum.enumValues)[number],
+) {
   // TODO: check if team username already exists before inserting
   // Remember to handle the error in the register form
   // #GoodFirstIssue
 
-  // TODO: Hash and salt the password before inserting
-  // #BadFirstIssue
-  await db.insert(teams).values({
-    username,
-    displayName,
-    password,
-    role: "user" as const,
-    interactionMode,
-    createTime: new Date(),
-  })
+  hash(password, 10, async (err, hash) => {
+    if (err) throw err;
+
+    await db.insert(teams).values({
+      username,
+      displayName,
+      password: hash,
+      role: "user" as const,
+      interactionMode,
+      createTime: new Date(),
+    });
+  });
 }
