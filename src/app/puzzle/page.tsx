ef57f26614589e032eba886c245e74042b8e13df
alex-j-wang <151220268+alex-j-wang@users.noreply.github.com>
2024-10-29 01:28:03 -0400

    38 puzzle page access for logged out users (#46)
    Hide puzzles from logged out users
    
    ---------
    
    Co-authored-by: alex-j-wang <alex-j-wang@users.noreply.github.com>

--- src/app/puzzle/page.tsx
+++ src/app/puzzle/page.tsx
@@ -1,17 +1,25 @@
 "use server";
+import { auth } from "@/auth";
 import { HUNT_START_TIME } from "@/hunt.config";
 import Link from "next/link";
 import { db } from "@/db/index";
 
 // NOTE: eventually, we probably want to create a layout for puzzle pages
 
-// TODO: should puzzles page be available to non-logged in users?
 export default async function Home() {
+  const session = await auth();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <p>This is the puzzle page.</p>
       {new Date() < HUNT_START_TIME ? (
         <p>The hunt has not started yet.</p>
+      ) : !session?.user?.id ? (
+        <div>
+          <Link href="/login" className="hover:underline">
+            Login
+          </Link>{" "}
+          to access puzzles.
+        </div>
       ) : (
         <>
           <p>Available puzzles:</p>
