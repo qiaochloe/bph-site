cbad6e4b668ae8c1cb5ef0b1a0e0577e6f538bc4
Alex Wang <alexander_wang@brown.edu>
2024-10-28 23:25:11 -0400

    Remove unused import

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -8,7 +8,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { sql } from "drizzle-orm";
-import { asc, desc, eq, or } from "drizzle-orm/expressions";
+import { asc, desc, eq } from "drizzle-orm/expressions";
 import { teams, guesses } from "~/server/db/schema";
 
 export const dynamic = "force-dynamic";
