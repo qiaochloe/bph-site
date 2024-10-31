29e1c87f8445b160b8e365f0ee59bb8bc1c024d0
Chloe Qiao <34826925+qiaochloe@users.noreply.github.com>
2024-10-31 04:09:43 -0400

    Open and close registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,24 @@
 "use server";
+import { REGISTRATION_START_TIME, REGISTRATION_END_TIME } from "@/hunt.config";
 import { RegisterForm } from "./RegisterForm";
 
 export default async function Home() {
+  if (new Date() < REGISTRATION_START_TIME) {
+    return (
+      <div className="flex h-screen flex-col items-center justify-center">
+        <p>Registration has not started yet.</p>
+      </div>
+    );
+  }
+
+  if (new Date() > REGISTRATION_END_TIME) {
+    return (
+      <div className="flex h-screen flex-col items-center justify-center">
+        <p>Registration has ended.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-screen flex-col items-center">
       <h1 className="p-4">Register!</h1>
--- src/hunt.config.ts
+++ src/hunt.config.ts
@@ -2,6 +2,6 @@
 // All times are in ISO
 // https://greenwichmeantime.com/articles/clocks/iso/
 export const REGISTRATION_START_TIME = new Date("2024-10-24T05:56:35Z");
-export const REGISTRATION_END_TIME = new Date("2024-11-24T05:56:35Z");
+export const REGISTRATION_END_TIME = new Date("2024-11-20T05:56:35Z");
 export const HUNT_START_TIME = new Date("2024-10-24T05:59:00Z");
-export const HUNT_END_TIME = new Date("2024-11-22T05:56:35Z");
+export const HUNT_END_TIME = new Date("2024-11-20T05:56:35Z");
