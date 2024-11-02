465dc7af720a280499515b2b7bf17d82a995a773
Alex Wang <alexander_wang@brown.edu>
2024-11-02 18:14:16 -0400

    Revert "Attempt to fix scroll"
    This reverts commit 3753232cb0afc3fbd50294859cf72a1463df5a63.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const session = await auth();
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <h1>Welcome!</h1>
       {!session?.user?.id && (
         <p>
