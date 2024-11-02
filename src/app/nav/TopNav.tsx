d6686243a32a6c8322d8911bef984ad6c79db5c4
Alex Wang <alexander_wang@brown.edu>
2024-11-02 17:22:42 -0400

    TopNav improvement

--- src/app/nav/TopNav.tsx
+++ src/app/nav/TopNav.tsx
@@ -5,38 +5,31 @@ import Link from "next/link";
 export async function TopNav() {
   const session = await auth();
   return (
-    <nav className="fixed flex w-full justify-between p-4">
-      <div className="flex w-full justify-between">
-        <div>
-          <Link href="/" className="hover:underline">
-            Home
+    <nav className="fixed flex w-full justify-between p-4 bg-slate-50 shadow-md z-50">
+      <div className="flex space-x-4">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/puzzle" className="hover:underline">
+          Puzzle
+        </Link>
+        <Link href="/teams" className="hover:underline">
+          Teams
+        </Link>
+      </div>
+      <div className="flex space-x-4">
+        {session?.user?.role === "admin" && (
+          <Link href="/admin" className="hover:underline">
+            Admin
           </Link>
-          &emsp;
-          <Link href="/puzzle" className="hover:underline">
-            Puzzle
+        )}
+        {session?.user?.id ? (
+          <LogoutButton />
+        ) : (
+          <Link href="/login" className="hover:underline">
+            Login
           </Link>
-          &emsp;
-          <Link href="/teams" className="hover:underline">
-            Teams
-          </Link>
-        </div>
-        <div>
-          {session?.user?.role === "admin" && (
-            <>
-              <Link href="/admin" className="hover:underline">
-                Admin
-              </Link>
-              <>&emsp;</>
-            </>
-          )}
-          {session?.user?.id ? (
-            <LogoutButton />
-          ) : (
-            <Link href="/login" className="hover:underline">
-              Login
-            </Link>
-          )}
-        </div>
+        )}
       </div>
     </nav>
   );
