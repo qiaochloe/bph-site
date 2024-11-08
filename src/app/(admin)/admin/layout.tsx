61db2e6adb7f24275a90ebee71264b47e0192994
Chloe Qiao <qiaochloe@gmail.com>
2024-11-08 17:06:02 -0500

    Move providers to the root layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,5 +1,4 @@
 import "~/styles/globals.css";
-import { Providers } from "~/app/nav/providers";
 import { AdminTopNav, AdminTopNavSpacer } from "~/app/nav/AdminTopNav";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -7,13 +6,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <Providers>
+    <>
       <AdminTopNav />
       <AdminTopNavSpacer />
       <main className="mx-5 mb-5 flex min-h-[calc(100vh-80px-5rem)]">
         {children}
       </main>
       <Toaster />
-    </Providers>
+    </>
   );
 }
