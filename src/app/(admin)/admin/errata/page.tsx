19ee82bbfb01509376ef98d9411c42550e726159
Brian Xu <xu.brian.s@gmail.com>
2024-11-08 04:24:28 -0500

    Use errataDialog in admin/errata to display existing errata (#114)

--- src/app/(admin)/admin/errata/page.tsx
+++ src/app/(admin)/admin/errata/page.tsx
@@ -10,10 +10,14 @@ export default async function Home() {
 
   const puzzleList = await db.query.puzzles.findMany();
 
+  const errataList = (await db.query.errata.findMany()).sort(
+    (a, b) => a.timestamp.getTime() - b.timestamp.getTime(),
+  );
+
   return (
     <div className="mx-auto flex max-w-4xl grow flex-col">
       <h1 className="mb-2">Errata</h1>
-      <ErratumForm puzzleList={puzzleList} />
+      <ErratumForm puzzleList={puzzleList} errataList={errataList} />
     </div>
   );
 }
