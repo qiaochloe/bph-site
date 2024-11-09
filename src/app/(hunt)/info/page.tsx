32149176427aae7c959f2639270bb141c1c8f6d6
Alex Wang <alexander_wang@brown.edu>
2024-11-08 22:08:35 -0500

    Puzzlethon info

---
+++ src/app/(hunt)/info/page.tsx
@@ -0,0 +1,51 @@
+export default async function Home() {
+  return (
+    <div className="space-y-4 w-2/3 mx-auto">
+      <h1 className="text-center">Info!</h1>
+
+      <h2>What is this?</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>Puzzlethon is an event where we write, test, and publish an entire puzzlehunt in 24 hours.</li>
+        <li>It's light-hearted, low-stakes, and a great way to get into writing puzzles if you're a Brown/RISD student.</li>
+        <li>We'll also be livestreaming the event. If you're curious about taking a look behind the scenes to see what goes into writing a puzzlehunt, check it out!</li>
+        <li>
+          Making a puzzlehunt requires all kinds of people with many different skillsets: we need writers, programmers, artists, testers, and more. If you're interested in participating in Puzzlethon (and you're a Brown or RISD student), fill out this form!
+        </li>
+      </ul>
+
+      <h2>What's a puzzlehunt?</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>A puzzlehunt is an event where teams of people compete to solve puzzles. These puzzles can be anything: word games, logic puzzles, video games, physical objects, or in-person challenges.</li>
+        <li>For a (much larger) example, take a look at last year's Brown Puzzlehunt.</li>
+      </ul>
+
+      <h2>When is it?</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>Writing will start on November 23rd, at noon ET.</li>
+        <li>Puzzles will be released on November 24th, at noon ET.</li>
+      </ul>
+
+      <h2>Who are you?</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>We are Brown Puzzle Club, the home of the puzzlehunting community at Brown!</li>
+        <li>There's a little more information about us here.</li>
+      </ul>
+
+      <h2>Why are you doing this?</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>It'll be fun!</li>
+        <li>We like making things.</li>
+        <li>It's a great way for new writers to learn puzzle-writing.</li>
+        <li>We want to test our new tech stack before winter break.</li>
+      </ul>
+
+      <h2>How can I be involved?</h2>
+      <ul className="list-disc list-inside space-y-2">
+        <li>If you're interested in participating in Puzzlethon (and you're a Brown or RISD student), fill out this form!</li>
+        <li>
+          If you're not a Brown or RISD student, you can check out our livestream, and solve the puzzles once they release. (Come back later for more information!)
+        </li>
+      </ul>
+    </div>
+  );
+} 
