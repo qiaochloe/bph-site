f202f25ac4fc5378b52b00d49eb56a5eb0a8c9bf
Chloe Qiao <qiaochloe@gmail.com>
2024-11-09 21:52:09 -0500

    Remove high-level async const

--- src/hunt.config.ts
+++ src/hunt.config.ts
@@ -66,14 +66,16 @@ export async function getNumberOfHintsRemaining(teamId: string) {
  * You should really avoid changing anything here after the hunt starts
  */
 
-const firstPuzzle = (await db.query.puzzles.findMany()).sort((a, b) =>
-  a.name.localeCompare(b.name),
-)[0];
-
 /** Puzzles available at the beginning of the hunt that will never need to be unlocked by the team.
  * This is currently set to the first puzzle in the database alphabetically.
  */
-export const INITIAL_PUZZLES: string[] = firstPuzzle ? [firstPuzzle.id] : [];
+export const INTIIAL_PUZZLES = async () => {
+  const firstPuzzle = (await db.query.puzzles.findMany()).sort((a, b) =>
+    a.name.localeCompare(b.name),
+  )[0];
+  const INITIAL_PUZZLES: string[] = firstPuzzle ? [firstPuzzle.id] : [];
+  return INITIAL_PUZZLES;
+};
 
 /** Returns a map for the next puzzles unlocked after a puzzle is solved.
  * This is currently set to a sequential unlock, sorted alphabetically by puzzle name.
