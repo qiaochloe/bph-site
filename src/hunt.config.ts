c70c9f6d523be6f97f697086729a9aa002405a42
Chloe Qiao <qiaochloe@gmail.com>
2024-11-20 00:57:26 -0500

    Make canViewSolutions singular

--- src/hunt.config.ts
+++ src/hunt.config.ts
@@ -135,7 +135,7 @@ export async function getNumberOfHintsRemaining(teamId: string) {
 /** Determines whether the user can view the solutions.
  * WARNING: make sure to exclude certain puzzles if the solutions aren't available
  */
-export async function canViewSolutions(puzzleId: string) {
+export async function canViewSolution(puzzleId: string) {
   // Get user id
   const session = await auth()!;
   if (!session?.user?.id) {
