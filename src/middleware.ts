192ab7befb7d16cda336996b62d38b25080267eb
Alex Wang <alexander_wang@brown.edu>
2024-12-10 00:45:09 -0500

    Middleware revert

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,38 +6,35 @@ export const { auth } = NextAuth(authConfig);
 
 export default auth(async (req) => {
   // Allow admins to access all pages
-  // if (req.auth?.user?.role === "admin") {
-  //   return;
-  // }
-  // // Protect admin pages from non-admin users
-  // if (
-  //   req.auth?.user?.role !== "admin" &&
-  //   req.nextUrl.pathname.startsWith("/admin")
-  // ) {
-  //   const newUrl = new URL("./", req.nextUrl.origin);
-  //   return Response.redirect(newUrl);
-  // }
+  if (req.auth?.user?.role === "admin") {
+    return;
+  }
+
+  // Protect admin pages from non-admin users
+  if (
+    req.auth?.user?.role !== "admin" &&
+    req.nextUrl.pathname.startsWith("/admin")
+  ) {
+    const newUrl = new URL("./", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+
   // Protect puzzle pages.
   // This only matches on /puzzle/puzzleId, not /puzzle/puzzleId/solution
   // or /puzzle/puzzleId/hint
-  // if (req.nextUrl.pathname.match(/^\/puzzle\/[^\/]+\/$/)) {
-  //   // After the hunt ends, all puzzles can be accessed
-  //   if (new Date() > HUNT_END_TIME) {
-  //     return;
-  //   }
-  //
-  //   // Unauthenticated users can't access puzzles
-  //   if (!req.auth?.user?.id) {
-  //     const newUrl = new URL("/login", req.nextUrl.origin);
-  //     return Response.redirect(newUrl);
-  //   }
-  //
-  //   // Authenticated users can't access puzzles before the hunt starts
-  //   if (new Date() < HUNT_START_TIME) {
-  //     const newUrl = new URL("/puzzle", req.nextUrl.origin);
-  //     return Response.redirect(newUrl);
-  //   }
-  // }
+  if (req.nextUrl.pathname.match(/^\/puzzle\/[^\/]+\/$/)) {
+    // Unauthenticated users
+    if (!req.auth?.user?.id) {
+      const newUrl = new URL("/login", req.nextUrl.origin);
+      return Response.redirect(newUrl);
+    }
+
+    // Before the hunt starts
+    if (new Date() < HUNT_START_TIME) {
+      const newUrl = new URL("/puzzle", req.nextUrl.origin);
+      return Response.redirect(newUrl);
+    }
+  }
 });
 
 export const config = {
